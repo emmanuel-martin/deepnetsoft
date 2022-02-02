05fab99dd71433a6d7f218d33d4c7246c6c3c923
Emmanuel Martin <helloiamemmanuel@gmail.com>
2022-02-02 10:19:29 +0530

    udpate

--- deepnetsoft/src/pages/login.js
+++ deepnetsoft/src/pages/login.js
@@ -4,25 +4,25 @@ import "../styles/login.css"
 
 const Login = () => {
 
-    return(
+    return (
         <div className="container">
 
             <div className="logo">
-                <img src={Logo} alt="logo"/>
+                <img src={Logo} alt="logo" />
             </div>
-           
+
             <div className="login-box">
                 <div className="heading">
                     <span>Login</span>
                 </div>
-                    <label>username: testdomain || password: testdomain</label>
-                    <input type="text" placeholder="Username" className="input-form" /><br></br>
-                    <input type="password" placeholder="Password" className="input-form" />
-                    <a href="/Dashboard" ><button className='button'>Submit</button></a>
+                <label>username: testdomain || password: testdomain</label>
+                <input type="text" placeholder="Username" className="input-form" /><br></br>
+                <input type="password" placeholder="Password" className="input-form" />
+                <a href="/Dashboard" ><button className='button'>Submit</button></a>
             </div>
         </div>
     );
 
 }
-
+udpate
 export default Login;
