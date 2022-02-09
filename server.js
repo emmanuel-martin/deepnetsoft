e19dc6f01a891b0ef9f7e2de6c6e2f1cbaec2e0d
Emmanuel Martin <helloiamemmanuel@gmail.com>
2022-02-09 19:49:51 +0530

    update

--- server.js
+++ server.js
@@ -10,22 +10,22 @@ app.use(express.json());
 app.post('/login', AuthorizeToken, (req, res) => {
 
     const username = req.body.username;
-    
-    const user = {user: username}
+
+    const user = { user: username }
     const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET);
-    res.json({accessToken: accessToken})
+    res.json({ accessToken: accessToken })
 })
 
 
 function AuthorizeToken(req, res, next) {
     const AuthHeader = req.headers['authorization'];
     const token = AuthHeader && AuthHeader.split(' ')[1]
-    if(token == null ){
+    if (token == null) {
         return res.sendStatus(401);
     }
 
     jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
-        if(err) return res.sendStatus(403);
+        if (err) return res.sendStatus(403);
         req.user = user;
         next()
     })
@@ -34,3 +34,4 @@ const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`server started on ${PORT}`));
 
+udpate
