77aec891c01c5613bfe72dd6bcede4e564a10090
Emmanuel Martin <helloiamemmanuel@gmail.com>
2022-02-13 16:37:44 +0530

    update

--- deepnetsoft/src/App.js
+++ deepnetsoft/src/App.js
@@ -2,36 +2,37 @@ import Dashboard from './pages/dashboard'
 import Login from './pages/login'
 import React, { useState } from 'react'
 
-import { BrowserRouter, Switch, Route} from "react-router-dom"
+import { BrowserRouter, Switch, Route } from "react-router-dom"
 import AddProducts from './pages/addproduct';
 function App() {
 
 
   return (
-    
-   <BrowserRouter>
 
-    
-    <Switch>
-     
-     
-      
-      <Route exact path='/'>
-        <Login />
-      </Route>
-      <Route exact path='/Login'>
-        <Login />
-      </Route>
-       <Route exact path='/Dashboard'>
-        <Dashboard />
-      </Route>
-    
-      <Route exact path='/addProducts'>
-        <AddProducts />
-      </Route>
-    </Switch>
-   </BrowserRouter>
+    <BrowserRouter>
+
+
+      <Switch>
+
+
+
+        <Route exact path='/'>
+          <Login />
+        </Route>
+        <Route exact path='/Login'>
+          <Login />
+        </Route>
+        <Route exact path='/Dashboard'>
+          <Dashboard />
+        </Route>
+
+        <Route exact path='/addProducts'>
+          <AddProducts />
+        </Route>
+      </Switch>
+    </BrowserRouter>
   );
 }
 
 export default App;
+update
